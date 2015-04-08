7c1d72f9b5f59fa918aed4f2fd8d1517db82ecf4
Celio NELSON <celio.nelson@icloud.com>
2015-04-08 14:59:51 +0200

    Make enemies visible in front of gems

--- js/engine.js
+++ js/engine.js
@@ -199,12 +199,12 @@ var Engine = (function(global) {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
+        gem.render();
         allEnemies.forEach(function(enemy) {
             enemy.render();
         });
 
         player.render();
-        gem.render();
     }
 
     /* This function is called by the reset function and draws the menu window
